29bf3a90d13d7edadf672e34bf7a4b53bc9502e3
adindaatriani08-cloud <adindaatriani08@gmail.com>
2026-03-26 16:23:35 +0700

    commit kedua

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/Dimsumku-Homemade/">
+      <BrowserRouter basename="/dimsumku-homemade/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
